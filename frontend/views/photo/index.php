03dff4a1664ceb5fcda7facaa18cbad5cd00ec32
valentin.musienko@gmail.com <valentin.musienko@gmail.com>
2018-05-20 18:38:58 +0300

    commit

---
+++ frontend/views/photo/index.php
@@ -0,0 +1,118 @@
+<div id="photo" class="section section_full">
+    <div class="content">
+        <div class="clearfix">
+            <div class="section_left">
+                <div class="section_top clearfix">
+                    <h2 class="photo_title">Фотографии</h2>
+                    <div class="search_interesting">
+                        <input type="text" placeholder="Что вам интересно?">
+                    </div>
+                    <div class="photo_btns">
+                        <a href="#" class="create_album_btn">Создать альбом</a>
+                        <a href="#" class="add_photo_btn">Добавить фото</a>
+                    </div>
+                </div>
+                <div class="albums">
+                    <h4>Мои альбомы</h4>
+                    <ul class="albums_list clearfix">
+                        <li>
+                            <a href="#">
+                                <img src="/images/photo1.jpg" alt="">
+                                <span class="hover_album">
+                                    <span class="album_name">Фото с работы</span>
+                                    <span class="album_pics_count">34</span>
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="/images/photo2.jpg" alt="">
+                                <span class="hover_album">
+                                    <span class="album_name">Фото с работы</span>
+                                    <span class="album_pics_count">34</span>
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="/images/photo3.jpg" alt="">
+                                <span class="hover_album">
+                                    <span class="album_name">Фото с работы</span>
+                                    <span class="album_pics_count">34</span>
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="photos">
+                    <h4>Мои фотографии</h4>
+                    <ul class="photos_list clearfix">
+                        <li>
+                            <a href="#">
+                                <img src="/images/photo1.jpg" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="/images/photo2.jpg" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="/images/photo3.jpg" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="/images/photo2.jpg" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="/images/photo3.jpg" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="/images/photo1.jpg" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="/images/photo3.jpg" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="/images/photo1.jpg" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="/images/photo2.jpg" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="/images/photo1.jpg" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="/images/photo2.jpg" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="/images/photo3.jpg" alt="">
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="section_right">
+                <?= \common\widgets\LastStreams::widget() ?>
+            </div>
+        </div>
+    </div>
+</div>
